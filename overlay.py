2396ec1f9caabb0311e6d5cbc367ec07d5858ea8
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-13 13:40:29 -0400

    Randomized volume and exports metadata

--- overlay.py
+++ overlay.py
@@ -1,13 +1,21 @@
 from pydub import AudioSegment
-from pydub.playback import play
+# from pydub.playback import play
 from random import randint
+from typing import Tuple
+import os
+import yaml
 
 
-def random_overlay(environment_path:str, overlay_path:str) -> AudioSegment:
+def random_overlay(environment_path:str, overlay_path:str) -> Tuple[AudioSegment, int, int]:
     environment = AudioSegment.from_wav(environment_path)
     overlay = AudioSegment.from_wav(overlay_path)
-    r = randint(0, len(environment) - len(overlay))
-    return environment.overlay(overlay, position=r)
+    rand_pos = randint(0, len(environment) - len(overlay)) # ms
+    rand_volume = randint(-10, 10) # dB
+    return (
+        environment.overlay(overlay + rand_volume, position=rand_pos),
+        rand_pos,
+        rand_volume
+    )
 
 
 if __name__ == '__main__':
@@ -16,5 +24,17 @@ if __name__ == '__main__':
     overlay_path = f'{path_stem}/Zastava M92/9 (1).wav'
     
     out_dir = 'overlay_tests'
+    os.remove(f'{out_dir}/metadata.yaml')
     for i in range(5):
-        random_overlay(environment_path, overlay_path).export(f'{out_dir}/random_overlay{i}.wav', format='wav')
+        sound, pos, vol = random_overlay(environment_path, overlay_path)
+        
+        out_path = f'{out_dir}/random_overlay_{i}.wav'
+        sound.export(out_path, format='wav')
+        meta = {
+            out_path.split('.')[0]: {
+                'position_ms': pos,
+                'volume_db': vol
+            }
+        }
+        with open(f'{out_dir}/metadata.yaml', 'a') as f:
+            yaml.dump(meta, f)
