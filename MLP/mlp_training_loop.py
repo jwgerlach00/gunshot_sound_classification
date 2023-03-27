e9d7b097fee4097028c817c72cb88555e7ceaa03
Andrew Jevitt <51527902+andrewjevitt@users.noreply.github.com>
2023-03-27 18:55:11 -0400

    msg

--- MLP/mlp_training_loop.py
+++ MLP/mlp_training_loop.py
@@ -1,18 +1,19 @@
-from helpers.preprocessing import get_imu_data
+from helpers.preprocessing import get_imu_data,get_model_params
 from mlp_architecture import MLPModel, MLPDataset
 from helpers import TrainingLoop
 import joblib
 
 if __name__ == '__main__':
-    hyperparams, imu, ann = get_imu_data()
+    hyperparams = get_model_params()
+    imu, ann = get_imu_data() #gets data
 
     train = True #set to train and save, or load and eval
     if train == True:
         training_loop = TrainingLoop(MLPModel, MLPDataset, hyperparams)
         training_loop.training_loop(imu, ann)
-        joblib.dump(training_loop, 'mlp.joblib')
+        joblib.dump(training_loop, 'mlp.joblib') #save model
     else:
-        training_loop = joblib.load('mlp.joblib')
+        training_loop = joblib.load('mlp.joblib') 
 
     print('Train accuracy: {0}%\nVal accuracy: {1}%'.format(training_loop.train_acc, training_loop.val_acc))
     training_loop.plot_loss()
