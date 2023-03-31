65baf494a68421f5beb9fb72f13715a25498c1e5
Andrew Jevitt <51527902+andrewjevitt@users.noreply.github.com>
2023-03-31 19:35:48 -0400

    simplified model

--- MLP/mlp_architecture.py
+++ MLP/mlp_architecture.py
@@ -7,27 +7,16 @@ class MLPModel(nn.Module):
     
     def __init__(self, hyperparams:dict):
         super(MLPModel, self).__init__()
-        hidden_layer_sizes = hyperparams['mlp']['hidden_layers']
         
         self.relu = nn.ReLU() # Activation function
-        self.input = nn.Linear(hyperparams['window_size'], hidden_layer_sizes[0])
+        self.input = nn.Linear(hyperparams['window_size'], 20)
 
-        self.hidden_layers = []
-        for i in range(len(hidden_layer_sizes)):
-            if i < len(hidden_layer_sizes) - 1:
-                self.hidden_layers.append(nn.Linear(hidden_layer_sizes[i], hidden_layer_sizes[i+1])\
-                    .to(device=MLPModel.device))
-            else:
-                self.hidden_layers.append(nn.Linear(hidden_layer_sizes[i], hyperparams['num_classes'])\
-                    .to(device=MLPModel.device))
+        self.hidden_layer = nn.Linear(20, hyperparams['num_classes'])
         self.output = nn.Softmax(dim=1)
         
     def forward(self, x):
-        x = self.input(x)
-        
-        for hidden_layer in self.hidden_layers:
-            x = self.relu(x)
-            x = hidden_layer(x) # No relu at end because output is softmax
+        x = self.relu(self.input(x))
+        x = self.hidden_layer(x) # No relu at end because output is softmax
 
         return self.output(x).view(-1)
 
--- MLP/mlp_training_loop.py
+++ MLP/mlp_training_loop.py
@@ -2,6 +2,7 @@ from helpers.preprocessing import get_model_params
 from mlp_architecture import MLPModel, MLPDataset
 from helpers import TrainingLoop
 import joblib
+from torchsummary import summary
 
 if __name__ == '__main__':
     hyperparams = get_model_params()
@@ -9,10 +10,11 @@ if __name__ == '__main__':
     train = True #set to train and save, or load and eval
     if train == True:
         training_loop = TrainingLoop(MLPModel, MLPDataset, hyperparams)
+        summary(training_loop.model,(32,100))
         training_loop.training_loop()
         joblib.dump(training_loop, 'mlp.joblib') #save model
     else:
         training_loop = joblib.load('mlp.joblib') 
 
-    print('Train accuracy: {0}%\nVal accuracy: {1}%'.format(training_loop.train_acc, training_loop.val_acc))
+    #print('Train accuracy: {0}%\nVal accuracy: {1}%'.format(training_loop.train_acc, training_loop.val_acc))
     training_loop.plot_loss()
