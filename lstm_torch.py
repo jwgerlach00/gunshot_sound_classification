9e292f3d28a36adf2ae1ea10fe1a664e3f4caf9a
Jacob Gerlach <jwgerlach00@gmail.com>
2023-04-23 19:05:51 -0400

    Changed to add window to lstm_torch

--- lstm_torch.py
+++ lstm_torch.py
@@ -5,20 +5,16 @@ import numpy as np
 from typing import Tuple
 import joblib
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 
 class LSTMModel(nn.Module):
     def __init__(self, X_shape:Tuple[int, int, int]):
         super(LSTMModel, self).__init__()
-        self.lstm = nn.LSTM(X_shape[2], hidden_size =128,num_layers =1,dropout =0.2,bidirectional =True,batch_first=True)
-        self.fc1 = nn.Linear(128, 2048)
-        self.fc2 = nn.Linear(2048, 2048)
-        self.fc3 = nn.Linear(2048, 2048)
-        self.fc4 = nn.Linear(2048, 2048)
-        self.fc5 = nn.Linear(2048, 256)
-        self.fc6 = nn.Linear(256, 128)
-        self.fc7 = nn.Linear(128, X_shape[1])
-        
+        self.lstm = nn.LSTM(X_shape[2], hidden_size=256, num_layers=1, dropout=0.2, bidirectional=False,
+                            batch_first=True)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 1)
         
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
@@ -27,28 +23,25 @@ class LSTMModel(nn.Module):
         _, (h, _) = self.lstm(x)
         h = h.squeeze(0)
         x = self.relu(self.fc1(h))
-        x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-        x = self.relu(self.fc4(x))
-        x = self.relu(self.fc5(x))
-        x = self.relu(self.fc6(x))
-        x = self.fc7(x)
+        x = self.fc2(x)
         x = self.sigmoid(x)
-        return x[0]
+        return x
 
 
 class LSTMDataset(Dataset):
-    def __init__(self, X:np.ndarray, y:np.ndarray):
-        self.X = X.copy()
-        self.y = y.copy()
+    def __init__(self, X:np.ndarray, y:np.ndarray, window_size:int=10):
+        self.window_size = window_size
+        self.X = np.reshape(X.copy(), (-1, X.shape[-1]))
+        self.y = np.reshape(y.copy(), (-1, 1))
+        # self.y = y.copy()
     
     def __len__(self):
-        return self.X.shape[0]
+        return self.X.shape[0] - self.window_size
     
     def __getitem__(self, idx:int):
         return (
-            torch.tensor(self.X[idx]),
-            torch.tensor(self.y[idx]).to(torch.float32)
+            torch.tensor(self.X[idx:idx+self.window_size, :]),
+            torch.tensor(self.y[idx+self.window_size]).to(torch.float32)
         )
         
 def calc_acc(y, y_p):
@@ -77,7 +70,7 @@ if __name__ == '__main__':
 
     # Define train/val split
     num_samples = X.shape[0]
-    train_ratio = .2
+    train_ratio = .8
     split_index = int(num_samples*train_ratio)
 
     # Split X and y
@@ -92,10 +85,10 @@ if __name__ == '__main__':
     model = LSTMModel(X_train.shape)
 
     EPOCHS = 100
-    BATCH_SIZE = 64
+    BATCH_SIZE = 32
     # criterion = nn.CrossEntropyLoss(weight=distribution(y_train))
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     
     batch_dataloader = DataLoader(LSTMDataset(X_train, y_train), batch_size=BATCH_SIZE, shuffle=False)
     val_dataloader = DataLoader(LSTMDataset(X_val, y_val), batch_size=BATCH_SIZE, shuffle=False)
@@ -107,42 +100,47 @@ if __name__ == '__main__':
 
         model.train()
         train_loss_history = []
-        for X, y in batch_dataloader:
+        train_acc_history = []
+        for X, y in tqdm(batch_dataloader):
             
             y_p = model(X)
             loss = criterion(y_p, y)
-            print("\n",zeros_and_ones(y_p[0]),"%")
-            print("Accuracy",calc_acc(y, y_p).item())
-            print("Loss",loss.item())
+            # print("\n",zeros_and_ones(y_p[0]),"%")
+            # print("Accuracy",calc_acc(y, y_p).item())
+            # print("Loss",loss.item())
             
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             
             train_loss_history.append(loss.item())
+            train_acc_history.append(calc_acc(y, y_p).item())
             
         print(f'Train Loss: {np.mean(train_loss_history)}')
-        print(class_counts((y_p > 0.5).int()))
+        print(f'Train Accuracy: {np.mean(train_acc_history)}')
         
         model.eval()
         val_loss_history = []
-        for X, y in val_dataloader:
+        val_acc_history = []
+        for X, y in tqdm(val_dataloader):
             with torch.no_grad():
                 y_p = model(X)
                 loss = criterion(y_p, y)
                 val_loss_history.append(loss.item())
+                val_acc_history.append(calc_acc(y, y_p).item())
                
         print(f'Val Loss: {np.mean(val_loss_history)}')
-        if np.mean(val_loss_history) < best_validation_loss:
-            best_validation_loss = np.mean(val_loss_history)
-            joblib.dump(model, f'lstm_torch_train{train_loss_history[-1]}_val{val_loss_history[-1]}.joblib')
+        print(f'Val Accuracy: {np.mean(val_acc_history)}')
+        # if np.mean(val_loss_history) < best_validation_loss:
+        #     best_validation_loss = np.mean(val_loss_history)
+        #     joblib.dump(model, f'lstm_torch_train{train_loss_history[-1]}_val{val_loss_history[-1]}.joblib')
         #training_loop = joblib.load('mlpd.joblib') 
 
-    plt.figure()
-    plt.title('Loss curve')
-    plt.plot(range(len(train_loss_history)), train_loss_history, label='train loss')
-    plt.plot(range(len(val_loss_history)), val_loss_history, label='val loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
+    # plt.figure()
+    # plt.title('Loss curve')
+    # plt.plot(range(len(train_loss_history)), train_loss_history, label='train loss')
+    # plt.plot(range(len(val_loss_history)), val_loss_history, label='val loss')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Loss')
+    # plt.legend()
+    # plt.show()
